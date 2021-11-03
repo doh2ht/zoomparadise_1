f12f4fe6d7cccba3913ed02fcc6333ceb32dfbdb
doh2ht <47910376+doh2ht@users.noreply.github.com>
2021-11-03 19:17:10 +0900

    Add files via upload

---
+++ static/home.js
@@ -0,0 +1,432 @@
+var background_img = "./background/img_";
+var number = Math.floor(Math.random() * 50) + 1;
+var container = document.getElementById("container");
+background_img += number + ".png";
+container.style.backgroundImage = "url('" + background_img + "')";
+
+var number = Math.floor(Math.random() * 50) + 1;
+var mySrc = "./imageA/img_" + number + ".mp4";
+tmpElement = document.createElement("video");
+tmpElement.setAttribute("autoplay", "true");
+tmpElement.setAttribute("loop", "true");
+tmpElement.setAttribute("muted", "true");
+tmpElement.setAttribute("src", mySrc);
+
+var container = document.getElementById("myVideoTag");
+container.appendChild(tmpElement);
+
+var number = Math.floor(Math.random() * 50) + 1;
+var mySrc = "./imageB/img_" + number + ".mp4";
+
+tmpElement = document.createElement("video");
+tmpElement.setAttribute("autoplay", "true");
+tmpElement.setAttribute("loop", "true");
+tmpElement.setAttribute("src", mySrc);
+tmpElement.setAttribute("muted", "true");
+
+var container = document.getElementById("myVideoTag2");
+container.appendChild(tmpElement);
+
+
+
+
+$(".hover1").hide();
+$(".v1").mouseover(function () {$(".box_1").hide();$(".hover1").show();});
+$(".v1").mouseleave(function () {$(".box_1").show();$(".hover1").hide();});
+
+$(".hover2").hide();
+$(".v2").mouseover(function () {$(".box_1").hide();$(".hover2").show();});
+$(".v2").mouseleave(function () {$(".box_1").show();$(".hover2").hide();});
+
+$(".hover3").hide();
+$(".v3").mouseover(function () {$(".box_1").hide();$(".hover3").show();});
+$(".v3").mouseleave(function () {$(".box_1").show();$(".hover3").hide();});
+
+$(".hover4").hide();
+$(".v4").mouseover(function () {$(".box_1").hide();$(".hover4").show();});
+$(".v4").mouseleave(function () {$(".box_1").show();$(".hover4").hide();});
+
+$(".hover5").hide();
+$(".v5").mouseover(function () {$(".box_1").hide();$(".hover5").show();});
+$(".v5").mouseleave(function () {$(".box_1").show();$(".hover5").hide();});
+
+$(".hover6").hide();
+$(".v6").mouseover(function () {$(".box_1").hide();$(".hover6").show();});
+$(".v6").mouseleave(function () {$(".box_1").show();$(".hover6").hide();});
+
+$(".hover7").hide();
+$(".v7").mouseover(function () {$(".box_1").hide();$(".hover7").show();});
+$(".v7").mouseleave(function () {$(".box_1").show();$(".hover7").hide();});
+
+$(".hover8").hide();
+$(".v8").mouseover(function () {$(".box_1").hide();$(".hover8").show();});
+$(".v8").mouseleave(function () {$(".box_1").show();$(".hover8").hide();});
+
+$(".hover9").hide();
+$(".v9").mouseover(function () {$(".box_1").hide();$(".hover9").show();});
+$(".v9").mouseleave(function () {$(".box_1").show();$(".hover9").hide();});
+
+$(".hover10").hide();
+$(".v10").mouseover(function () {$(".box_1").hide();$(".hover10").show();});
+$(".v10").mouseleave(function () {$(".box_1").show();$(".hover10").hide();});
+
+$(".hover11").hide();
+$(".v11").mouseover(function () {$(".box_1").hide();$(".hover11").show();});
+$(".v11").mouseleave(function () {$(".box_1").show();$(".hover11").hide();});
+
+$(".hover12").hide();
+$(".v12").mouseover(function () {$(".box_1").hide();$(".hover12").show();});
+$(".v12").mouseleave(function () {$(".box_1").show();$(".hover12").hide();});
+
+$(".hover13").hide();
+$(".v13").mouseover(function () {$(".box_1").hide();$(".hover13").show();});
+$(".v13").mouseleave(function () {$(".box_1").show();$(".hover13").hide();});
+
+$(".hover14").hide();
+$(".v14").mouseover(function () {$(".box_1").hide();$(".hover14").show();});
+$(".v14").mouseleave(function () {$(".box_1").show();$(".hover14").hide();});
+
+$(".hover15").hide();
+$(".v15").mouseover(function () {$(".box_1").hide();$(".hover15").show();});
+$(".v15").mouseleave(function () {$(".box_1").show();$(".hover15").hide();});
+
+$(".hover16").hide();
+$(".v16").mouseover(function () {$(".box_1").hide();$(".hover16").show();});
+$(".v16").mouseleave(function () {$(".box_1").show();$(".hover16").hide();});
+
+$(".hover17").hide();
+$(".v17").mouseover(function () {$(".box_1").hide();$(".hover17").show();});
+$(".v17").mouseleave(function () {$(".box_1").show();$(".hover17").hide();});
+
+$(".hover18").hide();
+$(".v18").mouseover(function () {$(".box_1").hide();$(".hover18").show();});
+$(".v18").mouseleave(function () {$(".box_1").show();$(".hover18").hide();});
+
+$(".hover19").hide();
+$(".v19").mouseover(function () {$(".box_1").hide();$(".hover19").show();});
+$(".v19").mouseleave(function () {$(".box_1").show();$(".hover19").hide();});
+
+$(".hover20").hide();
+$(".v20").mouseover(function () {$(".box_1").hide();$(".hover20").show();});
+$(".v20").mouseleave(function () {$(".box_1").show();$(".hover20").hide();});
+
+$(".hover21").hide();
+$(".v21").mouseover(function () {$(".box_1").hide();$(".hover21").show();});
+$(".v21").mouseleave(function () {$(".box_1").show();$(".hover21").hide();});
+
+$(".hover22").hide();
+$(".v22").mouseover(function () {$(".box_1").hide();$(".hover22").show();});
+$(".v22").mouseleave(function () {$(".box_1").show();$(".hover22").hide();});
+
+$(".hover23").hide();
+$(".v23").mouseover(function () {$(".box_1").hide();$(".hover23").show();});
+$(".v23").mouseleave(function () {$(".box_1").show();$(".hover23").hide();});
+
+$(".hover24").hide();
+$(".v24").mouseover(function () {$(".box_1").hide();$(".hover24").show();});
+$(".v24").mouseleave(function () {$(".box_1").show();$(".hover24").hide();});
+
+$(".hover25").hide();
+$(".v25").mouseover(function () {$(".box_1").hide();$(".hover25").show();});
+$(".v25").mouseleave(function () {$(".box_1").show();$(".hover25").hide();});
+
+$(".hover26").hide();
+$(".v26").mouseover(function () {$(".box_1").hide();$(".hover26").show();});
+$(".v26").mouseleave(function () {$(".box_1").show();$(".hover26").hide();});
+
+$(".hover27").hide();
+$(".v27").mouseover(function () {$(".box_1").hide();$(".hover27").show();});
+$(".v27").mouseleave(function () {$(".box_1").show();$(".hover27").hide();});
+
+$(".hover28").hide();
+$(".v28").mouseover(function () {$(".box_1").hide();$(".hover28").show();});
+$(".v28").mouseleave(function () {$(".box_1").show();$(".hover28").hide();});
+
+$(".hover29").hide();
+$(".v29").mouseover(function () {$(".box_1").hide();$(".hover29").show();});
+$(".v29").mouseleave(function () {$(".box_1").show();$(".hover29").hide();});
+
+$(".hover30").hide();
+$(".v30").mouseover(function () {$(".box_1").hide();$(".hover30").show();});
+$(".v30").mouseleave(function () {$(".box_1").show();$(".hover30").hide();});
+
+$(".hover31").hide();
+$(".v31").mouseover(function () {$(".box_1").hide();$(".hover31").show();});
+$(".v31").mouseleave(function () {$(".box_1").show();$(".hover31").hide();});
+
+$(".hover32").hide();
+$(".v32").mouseover(function () {$(".box_1").hide();$(".hover32").show();});
+$(".v32").mouseleave(function () {$(".box_1").show();$(".hover32").hide();});
+
+$(".hover33").hide();
+$(".v33").mouseover(function () {$(".box_1").hide();$(".hover33").show();});
+$(".v33").mouseleave(function () {$(".box_1").show();$(".hover33").hide();});
+
+$(".hover34").hide();
+$(".v34").mouseover(function () {$(".box_1").hide();$(".hover34").show();});
+$(".v34").mouseleave(function () {$(".box_1").show();$(".hover34").hide();});
+
+$(".hover35").hide();
+$(".v35").mouseover(function () {$(".box_1").hide();$(".hover35").show();});
+$(".v35").mouseleave(function () {$(".box_1").show();$(".hover35").hide();});
+
+$(".hover36").hide();
+$(".v36").mouseover(function () {$(".box_1").hide();$(".hover36").show();});
+$(".v36").mouseleave(function () {$(".box_1").show();$(".hover36").hide();});
+
+$(".hover37").hide();
+$(".v37").mouseover(function () {$(".box_1").hide();$(".hover37").show();});
+$(".v37").mouseleave(function () {$(".box_1").show();$(".hover37").hide();});
+
+$(".hover38").hide();
+$(".v38").mouseover(function () {$(".box_1").hide();$(".hover38").show();});
+$(".v38").mouseleave(function () {$(".box_1").show();$(".hover38").hide();});
+
+$(".hover39").hide();
+$(".v39").mouseover(function () {$(".box_1").hide();$(".hover39").show();});
+$(".v39").mouseleave(function () {$(".box_1").show();$(".hover39").hide();});
+
+$(".hover40").hide();
+$(".v40").mouseover(function () {$(".box_1").hide();$(".hover40").show();});
+$(".v40").mouseleave(function () {$(".box_1").show();$(".hover40").hide();});
+
+
+$(".hover41").hide();
+$(".v41").mouseover(function () {$(".box_1").hide();$(".hover41").show();});
+$(".v41").mouseleave(function () {$(".box_1").show();$(".hover41").hide();});
+
+$(".hover42").hide();
+$(".v42").mouseover(function () {$(".box_1").hide();$(".hover42").show();});
+$(".v42").mouseleave(function () {$(".box_1").show();$(".hover42").hide();});
+
+$(".hover43").hide();
+$(".v43").mouseover(function () {$(".box_1").hide();$(".hover43").show();});
+$(".v43").mouseleave(function () {$(".box_1").show();$(".hover43").hide();});
+
+$(".hover44").hide();
+$(".v44").mouseover(function () {$(".box_1").hide();$(".hover44").show();});
+$(".v44").mouseleave(function () {$(".box_1").show();$(".hover44").hide();});
+
+$(".hover45").hide();
+$(".v45").mouseover(function () {$(".box_1").hide();$(".hover45").show();});
+$(".v45").mouseleave(function () {$(".box_1").show();$(".hover45").hide();});
+
+$(".hover46").hide();
+$(".v46").mouseover(function () {$(".box_1").hide();$(".hover46").show();});
+$(".v46").mouseleave(function () {$(".box_1").show();$(".hover46").hide();});
+
+$(".hover47").hide();
+$(".v47").mouseover(function () {$(".box_1").hide();$(".hover47").show();});
+$(".v47").mouseleave(function () {$(".box_1").show();$(".hover47").hide();});
+
+$(".hover48").hide();
+$(".v48").mouseover(function () {$(".box_1").hide();$(".hover48").show();});
+$(".v48").mouseleave(function () {$(".box_1").show();$(".hover48").hide();});
+
+$(".hover49").hide();
+$(".v49").mouseover(function () {$(".box_1").hide();$(".hover49").show();});
+$(".v49").mouseleave(function () {$(".box_1").show();$(".hover49").hide();});
+
+$(".hover50").hide();
+$(".v50").mouseover(function () {$(".box_1").hide();$(".hover50").show();});
+$(".v50").mouseleave(function () {$(".box_1").show();$(".hover50").hide();});
+
+$(".hover51").hide();
+$(".v51").mouseover(function () {$(".box_1").hide();$(".hover51").show();});
+$(".v51").mouseleave(function () {$(".box_1").show();$(".hover51").hide();});
+
+$(".hover52").hide();
+$(".v52").mouseover(function () {$(".box_1").hide();$(".hover52").show();});
+$(".v52").mouseleave(function () {$(".box_1").show();$(".hover52").hide();});
+
+$(".hover53").hide();
+$(".v53").mouseover(function () {$(".box_1").hide();$(".hover53").show();});
+$(".v53").mouseleave(function () {$(".box_1").show();$(".hover53").hide();});
+
+$(".hover54").hide();
+$(".v54").mouseover(function () {$(".box_1").hide();$(".hover54").show();});
+$(".v54").mouseleave(function () {$(".box_1").show();$(".hover54").hide();});
+
+$(".hover55").hide();
+$(".v55").mouseover(function () {$(".box_1").hide();$(".hover55").show();});
+$(".v55").mouseleave(function () {$(".box_1").show();$(".hover55").hide();});
+
+$(".hover56").hide();
+$(".v56").mouseover(function () {$(".box_1").hide();$(".hover56").show();});
+$(".v56").mouseleave(function () {$(".box_1").show();$(".hover56").hide();});
+
+$(".hover57").hide();
+$(".v57").mouseover(function () {$(".box_1").hide();$(".hover57").show();});
+$(".v57").mouseleave(function () {$(".box_1").show();$(".hover57").hide();});
+
+$(".hover58").hide();
+$(".v58").mouseover(function () {$(".box_1").hide();$(".hover58").show();});
+$(".v58").mouseleave(function () {$(".box_1").show();$(".hover58").hide();});
+
+$(".hover59").hide();
+$(".v59").mouseover(function () {$(".box_1").hide();$(".hover59").show();});
+$(".v59").mouseleave(function () {$(".box_1").show();$(".hover59").hide();});
+
+$(".hover60").hide();
+$(".v60").mouseover(function () {$(".box_1").hide();$(".hover60").show();});
+$(".v60").mouseleave(function () {$(".box_1").show();$(".hover60").hide();});
+
+$(".hover61").hide();
+$(".v61").mouseover(function () {$(".box_1").hide();$(".hover61").show();});
+$(".v61").mouseleave(function () {$(".box_1").show();$(".hover61").hide();});
+
+$(".hover62").hide();
+$(".v62").mouseover(function () {$(".box_1").hide();$(".hover62").show();});
+$(".v62").mouseleave(function () {$(".box_1").show();$(".hover62").hide();});
+
+$(".hover63").hide();
+$(".v63").mouseover(function () {$(".box_1").hide();$(".hover63").show();});
+$(".v63").mouseleave(function () {$(".box_1").show();$(".hover63").hide();});
+
+$(".hover64").hide();
+$(".v64").mouseover(function () {$(".box_1").hide();$(".hover64").show();});
+$(".v64").mouseleave(function () {$(".box_1").show();$(".hover64").hide();});
+
+$(".hover65").hide();
+$(".v65").mouseover(function () {$(".box_1").hide();$(".hover65").show();});
+$(".v65").mouseleave(function () {$(".box_1").show();$(".hover65").hide();});
+
+$(".hover66").hide();
+$(".v66").mouseover(function () {$(".box_1").hide();$(".hover66").show();});
+$(".v66").mouseleave(function () {$(".box_1").show();$(".hover66").hide();});
+
+$(".hover67").hide();
+$(".v67").mouseover(function () {$(".box_1").hide();$(".hover67").show();});
+$(".v67").mouseleave(function () {$(".box_1").show();$(".hover67").hide();});
+
+$(".hover68").hide();
+$(".v68").mouseover(function () {$(".box_1").hide();$(".hover68").show();});
+$(".v68").mouseleave(function () {$(".box_1").show();$(".hover68").hide();});
+
+$(".hover69").hide();
+$(".v69").mouseover(function () {$(".box_1").hide();$(".hover69").show();});
+$(".v69").mouseleave(function () {$(".box_1").show();$(".hover69").hide();});
+
+$(".hover70").hide();
+$(".v70").mouseover(function () {$(".box_1").hide();$(".hover70").show();});
+$(".v70").mouseleave(function () {$(".box_1").show();$(".hover70").hide();});
+
+$(".hover71").hide();
+$(".v71").mouseover(function () {$(".box_1").hide();$(".hover71").show();});
+$(".v71").mouseleave(function () {$(".box_1").show();$(".hover71").hide();});
+
+$(".hover72").hide();
+$(".v72").mouseover(function () {$(".box_1").hide();$(".hover72").show();});
+$(".v72").mouseleave(function () {$(".box_1").show();$(".hover72").hide();});
+
+$(".hover73").hide();
+$(".v73").mouseover(function () {$(".box_1").hide();$(".hover73").show();});
+$(".v73").mouseleave(function () {$(".box_1").show();$(".hover73").hide();});
+
+$(".hover74").hide();
+$(".v74").mouseover(function () {$(".box_1").hide();$(".hover74").show();});
+$(".v74").mouseleave(function () {$(".box_1").show();$(".hover74").hide();});
+
+$(".hover75").hide();
+$(".v75").mouseover(function () {$(".box_1").hide();$(".hover75").show();});
+$(".v75").mouseleave(function () {$(".box_1").show();$(".hover75").hide();});
+
+$(".hover76").hide();
+$(".v76").mouseover(function () {$(".box_1").hide();$(".hover76").show();});
+$(".v76").mouseleave(function () {$(".box_1").show();$(".hover76").hide();});
+
+$(".hover77").hide();
+$(".v77").mouseover(function () {$(".box_1").hide();$(".hover77").show();});
+$(".v77").mouseleave(function () {$(".box_1").show();$(".hover77").hide();});
+
+$(".hover78").hide();
+$(".v78").mouseover(function () {$(".box_1").hide();$(".hover78").show();});
+$(".v78").mouseleave(function () {$(".box_1").show();$(".hover78").hide();});
+
+$(".hover79").hide();
+$(".v79").mouseover(function () {$(".box_1").hide();$(".hover79").show();});
+$(".v79").mouseleave(function () {$(".box_1").show();$(".hover79").hide();});
+
+$(".hover80").hide();
+$(".v80").mouseover(function () {$(".box_1").hide();$(".hover80").show();});
+$(".v80").mouseleave(function () {$(".box_1").show();$(".hover80").hide();});
+
+$(".hover81").hide();
+$(".v81").mouseover(function () {$(".box_1").hide();$(".hover81").show();});
+$(".v81").mouseleave(function () {$(".box_1").show();$(".hover81").hide();});
+
+$(".hover82").hide();
+$(".v82").mouseover(function () {$(".box_1").hide();$(".hover82").show();});
+$(".v82").mouseleave(function () {$(".box_1").show();$(".hover82").hide();});
+
+$(".hover83").hide();
+$(".v83").mouseover(function () {$(".box_1").hide();$(".hover83").show();});
+$(".v83").mouseleave(function () {$(".box_1").show();$(".hover83").hide();});
+
+$(".hover84").hide();
+$(".v84").mouseover(function () {$(".box_1").hide();$(".hover84").show();});
+$(".v84").mouseleave(function () {$(".box_1").show();$(".hover84").hide();});
+
+$(".hover85").hide();
+$(".v85").mouseover(function () {$(".box_1").hide();$(".hover85").show();});
+$(".v85").mouseleave(function () {$(".box_1").show();$(".hover85").hide();});
+
+$(".hover86").hide();
+$(".v86").mouseover(function () {$(".box_1").hide();$(".hover86").show();});
+$(".v86").mouseleave(function () {$(".box_1").show();$(".hover86").hide();});
+
+$(".hover87").hide();
+$(".v87").mouseover(function () {$(".box_1").hide();$(".hover87").show();});
+$(".v87").mouseleave(function () {$(".box_1").show();$(".hover87").hide();});
+
+$(".hover88").hide();
+$(".v88").mouseover(function () {$(".box_1").hide();$(".hover88").show();});
+$(".v88").mouseleave(function () {$(".box_1").show();$(".hover88").hide();});
+
+$(".hover89").hide();
+$(".v89").mouseover(function () {$(".box_1").hide();$(".hover89").show();});
+$(".v89").mouseleave(function () {$(".box_1").show();$(".hover89").hide();});
+
+$(".hover90").hide();
+$(".v90").mouseover(function () {$(".box_1").hide();$(".hover90").show();});
+$(".v90").mouseleave(function () {$(".box_1").show();$(".hover90").hide();});
+
+$(".hover91").hide();
+$(".v91").mouseover(function () {$(".box_1").hide();$(".hover91").show();});
+$(".v91").mouseleave(function () {$(".box_1").show();$(".hover91").hide();});
+
+$(".hover92").hide();
+$(".v92").mouseover(function () {$(".box_1").hide();$(".hover92").show();});
+$(".v92").mouseleave(function () {$(".box_1").show();$(".hover92").hide();});
+
+$(".hover93").hide();
+$(".v93").mouseover(function () {$(".box_1").hide();$(".hover93").show();});
+$(".v93").mouseleave(function () {$(".box_1").show();$(".hover93").hide();});
+
+$(".hover94").hide();
+$(".v94").mouseover(function () {$(".box_1").hide();$(".hover94").show();});
+$(".v94").mouseleave(function () {$(".box_1").show();$(".hover94").hide();});
+
+$(".hover95").hide();
+$(".v95").mouseover(function () {$(".box_1").hide();$(".hover95").show();});
+$(".v95").mouseleave(function () {$(".box_1").show();$(".hover95").hide();});
+
+$(".hover96").hide();
+$(".v96").mouseover(function () {$(".box_1").hide();$(".hover96").show();});
+$(".v96").mouseleave(function () {$(".box_1").show();$(".hover96").hide();});
+
+$(".hover97").hide();
+$(".v97").mouseover(function () {$(".box_1").hide();$(".hover97").show();});
+$(".v97").mouseleave(function () {$(".box_1").show();$(".hover97").hide();});
+
+$(".hover98").hide();
+$(".v98").mouseover(function () {$(".box_1").hide();$(".hover98").show();});
+$(".v98").mouseleave(function () {$(".box_1").show();$(".hover98").hide();});
+
+$(".hover99").hide();
+$(".v99").mouseover(function () {$(".box_1").hide();$(".hover99").show();});
+$(".v99").mouseleave(function () {$(".box_1").show();$(".hover99").hide();});
+
+$(".hover100").hide();
+$(".v100").mouseover(function () {$(".box_1").hide();$(".hover100").show();});
+$(".v100").mouseleave(function () {$(".box_1").show();$(".hover100").hide();});
